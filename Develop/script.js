15326f9987e04f902311137c5e91bead446d5a0b
atengelsgjerd <atengelsgjerd@gmail.com>
2024-03-21 14:43:43 -0500

    Completed README section and wrote notes in the JavaScript section explaining the steps.

--- Develop/script.js
+++ Develop/script.js
@@ -8,14 +8,18 @@ const collectEmployees = function () {
 
   let loopVal = true; //loop will run as long as it returns true
 
-  while (loopVal) { 
+  /*In this loop, prompt messages will pop up on website asking user to input data. Separate pop ups will appear consecutively for
+  the user to enter a first name, a last name, and a salary amount. After each Enter Salary pop up, a confirmation box will appear 
+  asking user if they are all finished. If they press OK, the loop will continue andthey will continue to see pop ups appear to enter data. 
+  If they click cancel, the loop will end.*/
+  while (loopVal) {  
     const firstName = prompt("Enter First Name");
     const lastName = prompt("Enter Last Name");
     let mySalary = parseFloat(prompt("Enter Your Salary"));
-    if (isNaN(mySalary)) {
+    if (isNaN(mySalary)) {//converts data entered to a number.
       mySalary = 0
     }
-    const employee = {
+    const employee = { //employee object holding the information that will be put into the employees array.
       firstName: firstName,
       lastName: lastName,
       salary: mySalary,
@@ -33,16 +37,18 @@ const displayAverageSalary = function (employees) {
   let salarySum = 0;
 
   for (let employee of employees) {
-    salarySum = salarySum + employee.salary;
+    salarySum = salarySum + employee.salary; //Adds employee.salary data to salarySum each time user inputs the data.
   }
-  console.log("Average Salary:", salarySum/employees.length); 
+  console.log("Average Salary:", salarySum/employees.length);  //This will display the average salary by dividing the total of what was entered by the how many employees were added.
 }
 
 // Select a random employee
-const getRandomEmployee = function (employees) {
+const getRandomEmployee = function (employees) { 
+   /*Math.random generates a random decimal between 1 and 0 and multiplies it by the number of employees in the array.
+   Math.floor rounds it down to the nearest number so that it will be one of the numbers in the array and not a decimal.*/
   const randomIndex = Math.floor(Math.random() * employees.length);
-  const randomEmployee = employees[randomIndex];
-  console.log(`Congratulations ${randomEmployee.firstName} ${randomEmployee.lastName}, you've won!`);
+  const randomEmployee = employees[randomIndex]; //retrieves employee object from the employees array and assigns random employee selected to randomEmployee variable.
+  console.log(`Congratulations ${randomEmployee.firstName} ${randomEmployee.lastName}, you've won!`); //displays this message alerting of the employee who has won the random drawing.
   
   // TODO: Select and display a random employee
 }   
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
+# Employee Payroll Tracking webservice
 
+## Description
+
+This is a website that allows Employers to input the name of their employees and their salaries so that it can store the data and display it as well.
+This website will also display the average salary of the employees and picks an employee at random to receive a giveaway prize!
+
+
+## Installation
+
+N/A
+
+## Usage
+
+This webpage will be used for Employers who need a reliable place to store Employee data, specifically their names and salaries.
+
+## Credits
+
+This webpage used the following sources while building it:
+Xpert Learning Assistant via Bootcamp
+[MDN Web Docs](https://developer.mozilla.org/en-US/)
+
+## License
+
+N/A
+
+[Click here to access the website](https://atengelsgjerd.github.io/ATEportfolio/)
+
+![Portfolio Website](/Develop/images/Employee.payroll.tracker.png)
